95028416b6fcf98c5c0ff6e5eb05745a91910766
santhoshsaravanamuthu <santhoshs.csbs2020@citchennai.net>
2023-06-28 09:33:50 +0530

    first code

--- script.js
+++ script.js
@@ -10,141 +10,131 @@ var books = [
     { title: "Book 9", author: "Author 9", subject: "Subject 9", publishDate: "2021-09-06" },
     { title: "Book 10", author: "Author 10", subject: "Subject 10", publishDate: "2023-10-07" },
     { title: "Book 11", author: "Author 11", subject: "Subject 11", publishDate: "2022-11-08" },
-    { title: "Book 12", author: "Author 12", subject: "Subject 12", publishDate: "2021-12-09" },
-    
+    { title: "Book 12", author: "Author 12", subject: "Subject 12", publishDate: "2021-12-09" },   
   ];
-  function addBook() {
-    const title = document.getElementById("bookTitle").value;
-    const author = document.getElementById("bookAuthor").value;
-    const subject = document.getElementById("bookSubject").value;
-    const publishDate = document.getElementById("bookPublishDate").value;
-
-    if (title && author && subject && publishDate) {
-        const bookes = { title, author, subject, publishDate };
-        books.push(bookes); // Update book list with new book added
-        alert("successful");
-    }
-
-  }
-  
   var currentPage = 1;
   var booksPerPage = 10;
   
-  function displayBooks(booksToDisplay) {
-    var table = document.getElementById("book-table");
-    table.innerHTML = "";
-  
-    var headerRow = table.insertRow(0);
-    var headers = ["Title", "Author", "Subject", "Publish Date"];
-    for (var i = 0; i < headers.length; i++) {
-      var headerCell = headerRow.insertCell(i);
-      headerCell.innerHTML = headers[i];
-    }
-  
-    var startIndex = (currentPage - 1) * booksPerPage;
-    var endIndex = startIndex + booksPerPage;
-    var displayedBooks = booksToDisplay.slice(startIndex, endIndex);
-  
-    for (var j = 0; j < displayedBooks.length; j++) {
-      var book = displayedBooks[j];
-      var row = table.insertRow(j + 1);
-  
-      var titleCell = row.insertCell(0);
-      var authorCell = row.insertCell(1);
-      var subjectCell = row.insertCell(2);
-      var dateCell = row.insertCell(3);
-  
-      titleCell.innerHTML = book.title;
-      authorCell.innerHTML = book.author;
-      subjectCell.innerHTML = book.subject;
-      dateCell.innerHTML = book.publishDate;
-    }
-    
+function displayBooks(booksToDisplay) {
+  var table = document.getElementById("book-table");
+  table.innerHTML = "";
+  var headerRow = table.insertRow(0);
+  var headers = ["Title", "Author", "Subject", "Publish Date"];
+  for (var i = 0; i < headers.length; i++) {
+    var headerCell = headerRow.insertCell(i);
+    headerCell.innerHTML = headers[i];
   }
-  function renderCriteriaCounts(books) {
-    const criteriaCounts = {
-      title: {},
-      author: {},
-      subject: {},
-      publishDate: {},
-    };
-  
-    books.forEach((book) => {
-      incrementCriteriaCount(criteriaCounts.title, book.title);
-      incrementCriteriaCount(criteriaCounts.author, book.author);
-      incrementCriteriaCount(criteriaCounts.subject, book.subject);
-      incrementCriteriaCount(criteriaCounts.publishDate, book.publishDate);
-    });
-  
-    renderCount('#title-count', criteriaCounts.title);
-    renderCount('#author-count', criteriaCounts.author);
-    renderCount('#subject-count', criteriaCounts.subject);
-    renderCount('#publish-date-count', criteriaCounts.publishDate);
+  var startIndex = (currentPage - 1) * booksPerPage;
+  var endIndex = startIndex + booksPerPage;
+  var displayedBooks = booksToDisplay.slice(startIndex, endIndex);
+  for (var j = 0; j < displayedBooks.length; j++) {
+    var book = displayedBooks[j];
+    var row = table.insertRow(j + 1);
+    var titleCell = row.insertCell(0);
+    var authorCell = row.insertCell(1);
+    var subjectCell = row.insertCell(2);
+    var dateCell = row.insertCell(3);
+    titleCell.innerHTML = book.title;
+    authorCell.innerHTML = book.author;
+    subjectCell.innerHTML = book.subject;
+    dateCell.innerHTML = book.publishDate;
   }
+}
+function addBook() {
+  const title = document.getElementById("bookTitle").value;
+  const author = document.getElementById("bookAuthor").value;
+  const subject = document.getElementById("bookSubject").value;
+  const publishDate = document.getElementById("bookPublishDate").value;
+
+  if (title && author && subject && publishDate) {
+      const newBook = { title, author, subject, publishDate };
+      books.push(newBook); // Update book list with new book added
+      alert("successful");
+      applyFilters();
+  }
+
+}
+function renderCriteriaCounts(books) {
+  const criteriaCounts = {
+    title: {},
+    author: {},
+    subject: {},
+    publishDate: {},
+  };
   
-  function incrementCriteriaCount(criteria, value) {
+  books.forEach((book) => {
+    incrementCriteriaCount(criteriaCounts.title, book.title);
+    incrementCriteriaCount(criteriaCounts.author, book.author);
+    incrementCriteriaCount(criteriaCounts.subject, book.subject);
+    incrementCriteriaCount(criteriaCounts.publishDate, book.publishDate);
+  });
+  
+  renderCount('#title-count', criteriaCounts.title);
+  renderCount('#author-count', criteriaCounts.author);
+  renderCount('#subject-count', criteriaCounts.subject);
+  renderCount('#publish-date-count', criteriaCounts.publishDate);
+}
+function incrementCriteriaCount(criteria, value) {
+  if (criteria.hasOwnProperty(value)) {
+    criteria[value].count += 1;
+  } else {
+    criteria[value] = { count: 1 };
+  }
+}
+function renderCount(selector, criteria) {
+  const countElement = document.querySelector(selector);
+  let totalCount = 0;
+  
+  for (const value in criteria) {
     if (criteria.hasOwnProperty(value)) {
-      criteria[value].count += 1;
-    } else {
-      criteria[value] = { count: 1 };
+      const count = criteria[value].count;
+      totalCount += count;
     }
   }
   
-  function renderCount(selector, criteria) {
-    const countElement = document.querySelector(selector);
-    let totalCount = 0;
+  countElement.textContent = totalCount;
+}
+function applyFilters() {
+  var titleFilter = document.getElementById("title-filter").value.toLowerCase();
+  var authorFilter = document.getElementById("author-filter").value.toLowerCase();
+  var subjectFilter = document.getElementById("subject-filter").value.toLowerCase();
+  var dateFilter = document.getElementById("date-filter").value.toLowerCase();
+  var filteredBooks = books.filter(function (book) {
+    return (
+      book.title.toLowerCase().includes(titleFilter) && 
+      book.author.toLowerCase().includes(authorFilter) &&
+      book.subject.toLowerCase().includes(subjectFilter) &&
+      book.publishDate.toLowerCase().includes(dateFilter)
+    );
+  });
   
-    for (const value in criteria) {
-      if (criteria.hasOwnProperty(value)) {
-        const count = criteria[value].count;
-        totalCount += count;
-      }
+  renderPagination(filteredBooks.length);
+  displayBooks(filteredBooks);
+  renderCriteriaCounts(filteredBooks);    
+  addBook(filteredBooks);
+}
+  
+function changePage(pageNumber) {
+  currentPage = pageNumber;
+  applyFilters();
+}
+  
+function renderPagination(totalBooks) {
+  var totalPages = Math.ceil(totalBooks / booksPerPage);
+  var pagination = document.getElementById("pagination");
+  pagination.innerHTML = "";
+  
+  for (var i = 1; i <= totalPages; i++) {
+    var link = document.createElement("span");
+    link.textContent = i;
+    link.classList.add("pagination-link");
+    if (i === currentPage) {
+      link.classList.add("active");
     }
-  
-    countElement.textContent = totalCount;
-  }
-  function applyFilters() {
-    var titleFilter = document.getElementById("title-filter").value.toLowerCase();
-    var authorFilter = document.getElementById("author-filter").value.toLowerCase();
-    var subjectFilter = document.getElementById("subject-filter").value.toLowerCase();
-    var dateFilter = document.getElementById("date-filter").value.toLowerCase();
-  
-    var filteredBooks = books.filter(function (book) {
-      return (
-        book.title.toLowerCase().includes(titleFilter) && 
-        book.author.toLowerCase().includes(authorFilter) &&
-        book.subject.toLowerCase().includes(subjectFilter) &&
-        book.publishDate.toLowerCase().includes(dateFilter)
-      );
+    link.addEventListener("click", function () {
+      changePage(parseInt(this.textContent));
     });
-  
-    renderPagination(filteredBooks.length);
-    displayBooks(filteredBooks);
-    renderCriteriaCounts(books);
-    addBook(books);
+    pagination.appendChild(link);
   }
-  
-  function changePage(pageNumber) {
-    currentPage = pageNumber;
-    applyFilters();
-  }
-  
-  function renderPagination(totalBooks) {
-    var totalPages = Math.ceil(totalBooks / booksPerPage);
-    var pagination = document.getElementById("pagination");
-    pagination.innerHTML = "";
-  
-    for (var i = 1; i <= totalPages; i++) {
-      var link = document.createElement("span");
-      link.textContent = i;
-      link.classList.add("pagination-link");
-      if (i === currentPage) {
-        link.classList.add("active");
-      }
-      link.addEventListener("click", function () {
-        changePage(parseInt(this.textContent));
-      });
-      pagination.appendChild(link);
-    }
-  }
+}
 applyFilters();
